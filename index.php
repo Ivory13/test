91c73eac7df6074264f95f390c9c8815a35bca74
Ivory13 <marthafc@outlook.com>
2015-08-19 07:55:10 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+	<head>
+		<style>
+			body {
+
+				background: url('me.jpg')no-repeat center center fixed; 
+  				-webkit-background-size: cover;
+  				-moz-background-size: cover;
+  				-o-background-size: cover;
+  				background-size: cover;;
+				background-size: cover;
+			}
+
+			h1 {
+				color: white;
+				font-family: 'Helvetica Neue', Helvetica;
+				font-weight: 60;
+				font-size: 40px;
+				margin: 0px;
+			}
+
+			p {
+				color: white;
+				font-family: 'Helvetica Neue', Helvetica;
+				font-weight: 800;
+			}
+			.box {
+				background-color: rgba(30,30,30,0.5);
+				padding: 20px;
+				width:250px;
+				margin: 140px;
+				text-align: left;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div class= "box">	
+			<h1> 
+				<i>Martha Clark</i> 
+			</h1>
+
+			<p>
+				<i>Bio</i>
+			</p>
+
+			<p>
+				Registered Nurse, long-term care - Geriatrics. 
+				Language skills:Portuguese, Spanish, and English.
+				Pursuing Computer Science degree. 
+				Brazilian born, naturalized American. 
+				Lives in Nebraska.
+			</p>
+
+
+			<br />
+			<br />
+
+			<a href="http://github.com/KenMazaika">
+				<img src="cat.png" />
+			</a>
+
+		</div>
+	</body>
+
+</html>
